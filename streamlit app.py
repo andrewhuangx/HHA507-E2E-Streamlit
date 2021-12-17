7b81c4ce2cfbec59fccded5c2ab6b1b269b7316f
Andrew <andrewhuang58@gmail.com>
2021-12-16 23:15:04 -0500

    streamlit code

---
+++ streamlit app.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 15 15:05:10 2021
+
+@author: andre
+"""
+
+## Import the Required Packages
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+
+## Load in CSVs 
+
+@st.cache
+def load_hospitals():
+   hospital = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
+   return hospital
+@st.cache
+def load_inpatient():
+    inpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
+    return inpatient
+@st.cache
+def load_outpatient():
+    outpatient = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv')
+    return outpatient
+
+streamlit run st.title('HHA 507 Streamlit App')
+
+
+
+
+
+
+
+
+
+
+
+
+
